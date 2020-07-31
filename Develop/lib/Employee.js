0892434b3eb5c7dd912ad97380cc4e868712dec9
Trevor Otterson <totterson97@gmail.com>
2020-07-31 15:57:28 -0600

    all tests pass and function properly

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,10 +1,22 @@
-// TODO: Write code to define and export the Employee class
-const Employee = function(name, id, email) {
-    this.name = name
-    this.id = id
-    this.email = email
+// defines and exports the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name
+        this.id = id
+        this.email = email
+    }
+    getName() {
+        return this.name
+    }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }  
+    getRole() {
+        return "Employee"
+    }      
 }
-
-getName(name)
 
 module.exports = Employee
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,6 +1,17 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const Manager = function(officeNumber) {
-    this.officeNumber = officeNumber
+// defines and exports the Manager class.
+const Employee = require("./Employee")
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email)
+        this.officeNumber = officeNumber
+    }
+    getRole() {
+        return "Manager"
+    }
+    getOfficeNumber() {
+        return this.officeNumber
+    }
 }
 
 module.exports = Manager
