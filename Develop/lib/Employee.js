7347ca108d99ee9392ede6534941fe006c78f276
Trevor Otterson <totterson97@gmail.com>
2020-07-30 18:35:53 -0600

    added more information to each js file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,7 +1,10 @@
 // TODO: Write code to define and export the Employee class
-const Employee = function(name, id) {
+const Employee = function(name, id, email) {
     this.name = name
     this.id = id
+    this.email = email
 }
+
+getName(name)
 
 module.exports = Employee
